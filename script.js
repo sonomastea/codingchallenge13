36f0387667b3dee89b59b4aa78f8d0d975abc83d
sonomastea <sbstea@usf.edu>
2024-06-28 16:22:59 -0400

    intialize product displau and ass async function/error messages

--- script.js
+++ script.js
@@ -11,6 +11,29 @@ document.addEventListener('DOMContentLoaded', function() {
   const productPrice=document.getElementById('product-price')
   const previousButton=document.getElementById('previous')
   const nextbutton=document.getElementById('next')
-
+  //product display 
   
-}})
+  let currentIndex= 0;
+  let products=[];
+
+// async function loading and error messages
+
+async function asyncCall(product){
+  try{
+        
+  const  request= await fetch (apiURL);
+   if (!request.ok) throw new Error('Network did not respond')  
+  
+    products= await request.json();
+ if (products.length=0)  throw new Error ('Products not found');
+ 
+ displayProduct(currentIndex); 
+}  catch(error) {
+console.error('Error loading products');
+loadingElement.style.display='none';
+errorElement.style.display=('Error processing products, reload page')
+
+
+
+}}
+})
