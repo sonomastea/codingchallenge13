1d80277601067c98d2bb525fb3910875da14ab4e
sonomastea <sbstea@usf.edu>
2024-06-28 16:22:06 -0400

    add data display

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const apiURL='https://course-api.com/react-store-products';
+  //data
+  const loadingElement= document.getElementById('loading')
+  const errorElement=document.getElementById('error');
+  const productContainer=document.getElementById('product-container');
+  const productImage=document.getElementById('width', 600);
+  const imageSrc= document.src=document.getElementById("myImg").src="apiURL"
+  const productName= document.getElementById('product-name');
+  const productDescription=document.getElementById('product-description');
+  const productPrice=document.getElementById('product-price')
+  const previousButton=document.getElementById('previous')
+  const nextbutton=document.getElementById('next')
 
+  
+}})
